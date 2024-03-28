79ace20884ceb5a5833de18e796a3927ade64278
Akshita Das <75940178+akshita1809@users.noreply.github.com>
2024-03-28 21:58:36 +0530

    Update bisectfunction.py

--- bisectfunction.py
+++ bisectfunction.py
@@ -1,4 +1,4 @@
-#Write a Python program to locate the left insertion point for a specified value in sorted order.
+#Write a Python program to locate the left insertion point for a specified value in sorted order. what is left intersection ***
 import bisect
 def index(a, x):
     i = bisect.bisect_left(a, x)
